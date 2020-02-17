4b0179cbf8e55236c3793cd6946f7b61b3159cf5
SedanaYoga <9g.yoga27@gmail.com>
2020-02-17 14:04:09 +0700

    Added execute notebook

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ data_gen_args = dict(rotation_range = 0.2,
                     zoom_range = 0.05,
                     horizontal_flip = True,
                     fill_mode = 'nearest')
+
 myGene = trainGenerator(2, 'data/train', 'image', 'label', data_gen_args, save_to_dir = None)
 
 model = unet()
-mode_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor = 'loss', verbose = 1, save_best_only = True)
+model_checkpoint = ModelCheckpoint('unet_weights.hdf5', monitor = 'loss', verbose = 1, save_best_only = True)
 model.fit_generator(myGene, steps_per_epoch = 300, epochs = 1, callbacks = [model_checkpoint])
 
 testGene = testGenerator("data/test")

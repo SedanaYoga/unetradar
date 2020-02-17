ca01d2c8c36487a2873383d301142a4dc2748e2a
SedanaYoga <9g.yoga27@gmail.com>
2020-02-18 00:00:05 +0700

    Update 1000 augmentation images, overfit code

--- model.py
+++ model.py
@@ -8,18 +8,7 @@ from keras.optimizers import *
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as keras
 
-def dice_coef(y_true, y_pred):
-    smooth = 1
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-
-def dice_coef_loss(y_true, y_pred):
-    print('dice_loss')
-    return 1-dice_coef(y_true, y_pred)
-
-def unet(pretrained_weights = None, input_size = (256,256, 3)):
+def unet(pretrained_weights = None, input_size = (256,256, 1)):
     inputs = Input(input_size)
     #keras.layers.Conv2D(filters, kernel_size, strides=(1, 1), 
     #                   padding='valid', data_format=None, 
@@ -32,7 +21,7 @@ def unet(pretrained_weights = None, input_size = (256,256, 3)):
     # Rangkaian Extraction 1
     conv1 = Conv2D(64, 3 , activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
     conv1 = Conv2D(64, 3 , activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
-    pool1 = MaxPooling2D(poo_size = (2,2))(conv1)
+    pool1 = MaxPooling2D(pool_size = (2,2))(conv1)
 
     # Rangkaian Extraction 2
     conv2 = Conv2D(128, 3 , activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
@@ -42,13 +31,13 @@ def unet(pretrained_weights = None, input_size = (256,256, 3)):
     # Rangkaian Extraction 3
     conv3 = Conv2D(256, 3 , activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
     conv3 = Conv2D(256, 3 , activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-    pool3 = MaxPooling2D(pooling_size = (2,2))(conv3)
+    pool3 = MaxPooling2D(pool_size = (2,2))(conv3)
 
     # Rangkaian Extraction 4
     conv4 = Conv2D(512, 3 , activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
     conv4 = Conv2D(512, 3 , activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
     drop4 = Dropout(0.5)(conv4)
-    pool4 = MaxPooling2D(pooling_size = (2,2))(drop4)
+    pool4 = MaxPooling2D(pool_size = (2,2))(drop4)
 
     # Rangkaian Extraction 5
     conv5 = Conv2D(1024, 3 , activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
@@ -56,25 +45,25 @@ def unet(pretrained_weights = None, input_size = (256,256, 3)):
     drop5 = Dropout(0.5)(conv5)
 
     # Rangkaian Expansion 1
-    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2)))(drop5)
+    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
     merge6 = concatenate([drop4, up6], axis = 3)
     conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
     conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
 
     # Rangkaian Expansion 2
-    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2)))(conv6)
+    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
     merge7 = concatenate([conv3, up7], axis = 3)
     conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
     conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
 
     # Rangkaian Expansion 3
-    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2)))(conv7)
+    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
     merge8 = concatenate([conv2, up8], axis = 3)
     conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
     conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
 
     # Rangkaian Expansion 4
-    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2)))(conv8)
+    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
     merge9 = concatenate([conv1, up9], axis = 3)
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
@@ -86,7 +75,7 @@ def unet(pretrained_weights = None, input_size = (256,256, 3)):
     # Membuat Model 
     model = Model(input = inputs, output = conv10)
 
-    model.compile(optimizer = Adam(Lr = 1e-4), loss = dice_coef_loss, metrics = ['accuracy'])
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
 
     if(pretrained_weights):
         model.load_weights(pretrained_weights)

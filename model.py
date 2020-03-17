a9e0a71e1e1715a3dac3447118fc718ee84e0f87
SedanaYoga <9g.yoga27@gmail.com>
2020-03-18 05:23:06 +0700

    Update monitor files

--- model.py
+++ model.py
@@ -76,7 +76,7 @@ def unet(pretrained_weights = None, input_size = (256,256, 1)):
     model = Model(input = inputs, output = conv10)
 
     model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
-
+    model.summary()
     if(pretrained_weights):
         model.load_weights(pretrained_weights)
     return model
